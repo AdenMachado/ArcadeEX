9508c57d2fe8436671124a7ae151e6e92e7361da
adenmachado <karolius009@gmail.com>
2026-02-23 18:47:53 +0500

    Added primitive economic

--- data.py
+++ data.py
@@ -1,2 +1,2 @@
-score, upgr11, upgr12, upgr13, upgr14  = 3999, 0, 0, 0, 0,
+score, upgr11, upgr12, upgr13, upgr14  = 3000000000, 0, 0, 0, 0,
 upgr11costs, upgr12costs, upgr13costs, upgr14costs  = 32, 8192, 67108864, 68719476736,
