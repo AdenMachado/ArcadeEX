bc353f32c42833c61d2b3a85cb99c33bd9acfb9d
adenmachado <karolius009@gmail.com>
2026-02-27 18:37:45 +0500

    Added HUD design, sounds, effects

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-score = 999_999
+score = 0
 upgr11, upgr12, upgr13, upgr14  = 0, 0, 0, 0,
 upgr11costs, upgr12costs, upgr13costs, upgr14costs  = 20, 150, 1_200, 10_000,
 upgr21, upgr22, upgr23, upgr24 = 0, 0, 0, 0
